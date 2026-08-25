8a2931542b10cedaafd07d40a168fbe2759a5ebd
Sve-AR <sverreabdouryeng@gmail.com>
2026-08-25 08:37:55 +0200

    huh

---
+++ Laboppg.1/Hus.js
@@ -0,0 +1,11 @@
+"use const"
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(20, 20);
+ctx.lineTo(20, 100);
+ctx.lineTo(70, 100);
+ctx.strokeStyle = "red";
+ctx.stroke();
